23df71e2cc4cad4922639c9dad34b3ee7782f372
DESKTOP-C0DN2AC\pms <pms000723@gmail.com>
2024-05-30 17:11:42 +0900

    [Algorithm] - 프로그래머스 첫 번째로 나오는 음수

--- Algorithm/src/main/java/org/example/Main.java
+++ Algorithm/src/main/java/org/example/Main.java
@@ -2,13 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        // Solution1 Test
+//        Solution solution = new Solution();
+//        int[] num_list = {1, 2, 3, 4, 5};
+//        int n = num_list.length;
+//        int[] result = solution.solution1(num_list, n);
+//        System.out.println("Result:");
+//        for (int num : result) {
+//            System.out.print(num + " ");
+//        }
+
+        // Solution2 Test
         Solution solution = new Solution();
-        int[] num_list = {1, 2, 3, 4, 5};
-        int n = num_list.length;
-        int[] result = solution.solution(num_list, n);
-        System.out.println("Result:");
-        for (int num : result) {
-            System.out.print(num + " ");
-        }
+        int[] num_list = {2, 1, -6, 4, 5};
+        int result = solution.solution2(num_list);
+
+        System.out.println("Result:" + result);
     }
 }
--- Algorithm/src/main/java/org/example/Solution.java
+++ Algorithm/src/main/java/org/example/Solution.java
@@ -1,10 +1,21 @@
 package org.example;
 
 public class Solution {
-    public int[] solution(int[] num_list, int n) {
+    public int[] solution1(int[] num_list, int n) {
         int[] answer = new int[n];
         for(int i=0; i<answer.length; i++){
             answer[i] = num_list[i];
+        }
+        return answer;
+    }
+
+    public int solution2(int[] num_list) {
+        int answer = -1;
+        for(int i = 0; i<num_list.length;i++){
+            if(num_list[i]<0){
+                answer = i;
+                break;
+            }
         }
         return answer;
     }
