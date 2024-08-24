e1af32ee4def60445423e4b418166eec7953db3c
DESKTOP-C0DN2AC\pms <pms000723@gmail.com>
2024-08-24 20:20:16 +0900

    [Algorithm] - 프로그래머스 길이에 따른 연산

---
+++ Algorithm/solution/LengthBasedOperation.java
@@ -0,0 +1,22 @@
+package algorithm.solution;
+
+class LengthBasedOperation {
+    public int solution(int[] num_list) {
+
+        int sum = 0;
+        int mul = 1;
+        int answer = 0;
+
+        for(int i = 0; i<num_list.length;i++){
+            if(num_list.length>=11){
+                sum += num_list[i];
+                answer = sum;
+            }
+            else {
+                mul *= num_list[i];
+                answer = mul;
+            }
+        }
+        return answer;
+    }
+}
