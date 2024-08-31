e13c60a1825a272b4d8b80a9fb2006fab5b26a29
DESKTOP-C0DN2AC\pms <pms000723@gmail.com>
2024-08-31 14:45:13 +0900

    [Algorithm] - 프로그래머스 n개 간격의 원소들

---
+++ Algorithm/solution/ElementSelector.java
@@ -0,0 +1,18 @@
+package algorithm.solution;
+
+public class ElementSelector {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer;
+        if (num_list.length % n == 0) {
+            answer = new int[num_list.length / n];
+        } else {
+            answer = new int[num_list.length / n + 1];
+        }
+
+        int index = 0;
+        for (int i = 0; i < num_list.length; i += n) {
+            answer[index++] = num_list[i];
+        }
+        return answer;
+    }
+}
