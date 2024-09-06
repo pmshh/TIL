68a65fecac094834d4f7042325f508fee950670e
DESKTOP-C0DN2AC\pms <pms000723@gmail.com>
2024-09-06 19:51:52 +0900

    [Algorithm] - 프로그래머스 부분 글자 이어 붙여 문자열 만들기

---
+++ Algorithm/solution/StringConcatenator.java
@@ -0,0 +1,13 @@
+package algorithm.solution;
+
+public class StringConcatenator {
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
+
+        for(int i=0; i<index_list.length; i++){
+            answer += my_string.charAt(index_list[i]);
+        }
+
+        return answer;
+    }
+}
