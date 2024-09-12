83e57f2b6954729222864456eeb89f5bdb3f7c82
DESKTOP-IJ9105T\PMS <pms000723@gmail.com>
2024-09-12 22:51:10 +0900

    [Algorithm] - 프로그래머스 이어 붙인 수

---
+++ Algorithm/solution/NumberCombiner.java
@@ -0,0 +1,18 @@
+package algorithm.solution;
+
+public class NumberCombiner {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String odd = "";
+        String even = "";
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                even += Integer.toString(num_list[i]);
+            } else {
+                odd += Integer.toString(num_list[i]);
+            }
+        }
+        answer = Integer.parseInt(even) + Integer.parseInt(odd);
+        return answer;
+    }
+}
