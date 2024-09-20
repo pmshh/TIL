3f26f24ee0fcb4a3c80f95482c3a197f6b23c65b
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-09-20 23:07:11 +0900

    [Algorithm] - 프로그래머스 홀짝 구분하기

---
+++ Algorithm/solution/OddEvenChecker.java
@@ -0,0 +1,15 @@
+package algorithm.solution;
+
+import java.util.Scanner;
+
+public class OddEvenChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.print(n+" is even");
+        }else{
+            System.out.print(n+" is odd");
+        }
+    }
+}
