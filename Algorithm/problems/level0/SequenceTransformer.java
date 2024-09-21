f23a8f37b492f949755ac16fc49f4c9cc06e9192
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-09-21 23:27:46 +0900

    [Algorithm] - 프로그래머스 조건에 맞게 수열 변환하기 3

---
+++ Algorithm/problems/level0/SequenceTransformer.java
@@ -0,0 +1,17 @@
+package algorithm.problems.level0;
+
+public class SequenceTransformer {
+    public int[] solution(int[] arr, int k) {
+        int[] answer = new int[arr.length];
+
+        for(int i=0; i<arr.length; i++){
+            if(k%2==0){
+                answer[i]=arr[i]+k;
+            }else{
+                answer[i]=arr[i]*k;
+            }
+        }
+
+        return answer;
+    }
+}
