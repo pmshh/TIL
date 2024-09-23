41847124804695542cecd1ba53d6f97f4730379b
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-09-23 22:07:38 +0900

    [Algorithm] - 프로그래머스 부분 문자열

---
+++ Algorithm/problems/level0/SubstringChecker.java
@@ -0,0 +1,11 @@
+package algorithm.problems.level0;
+
+public class SubstringChecker {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        if(str2.contains(str1)){
+            answer = 1;
+        }
+        return answer;
+    }
+}
