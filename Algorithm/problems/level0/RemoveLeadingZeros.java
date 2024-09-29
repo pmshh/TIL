fa7a64e21dd14786a4efdb63f3e6286fda36fa1a
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-09-29 22:37:39 +0900

    [Algorithm] - 프로그래머스 0 떼기

---
+++ Algorithm/problems/level0/RemoveLeadingZeros.java
@@ -0,0 +1,9 @@
+package algorithm.problems.level0;
+
+public class RemoveLeadingZeros {
+    public String solution(String n_str) {
+        int i = Integer.parseInt(n_str);
+        String answer = i+"";
+        return answer;
+    }
+}
