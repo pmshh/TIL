b964c2b24b2d316d846a13ca57d460ca7b3ab682
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-10-03 19:26:51 +0900

    [Algorithm] - 프로그래머스 문자열로 변환

---
+++ Algorithm/problems/level0/StringConverter.java
@@ -0,0 +1,8 @@
+package algorithm.problems.level0;
+
+public class StringConverter {
+    public String solution(int n) {
+        String answer = Integer.toString(n);
+        return answer;
+    }
+}
