5af55b9d439b65df6be4a163fc2f61daff434790
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-10-08 23:47:56 +0900

    [Algorithm] - 프로그래머스 문자열 정수의 합

---
+++ Algorithm/problems/level0/SumOfStringDigits.java
@@ -0,0 +1,13 @@
+package algorithm.problems.level0;
+
+public class SumOfStringDigits {
+    public int solution(String num_str) {
+        int answer = 0;
+
+        for(int i=0; i<num_str.length(); i++){
+            answer += Integer.parseInt(num_str.substring(i, i+1));
+        }
+
+        return answer;
+    }
+}
