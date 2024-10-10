40cab47c8ddc5d6737299ad482f31438c9a7faeb
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-10-10 20:34:45 +0900

    [Algorithm] - 프로그래머스 뒤에서 5등까지

---
+++ Algorithm/problems/level0/BottomFive.java
@@ -0,0 +1,14 @@
+package algorithm.problems.level0;
+
+import java.util.Arrays;
+
+public class BottomFive {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[5];
+        Arrays.sort(num_list);
+        for(int i=0; i<5;i++){
+            answer[i] = num_list[i];
+        }
+        return answer;
+    }
+}
