7319b5d29e2e07cbb9722dc4048d0766cd52f701
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-10-15 22:21:22 +0900

    [Algorithm] - 프로그래머스 문자열 출력하기

---
+++ Algorithm/problems/level0/StringPrinter.java
@@ -0,0 +1,11 @@
+package algorithm.problems.level0;
+
+import java.util.Scanner;
+
+public class StringPrinter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
