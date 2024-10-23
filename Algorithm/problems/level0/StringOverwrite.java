d481c419329a6f596e4e53e72ef4f824c263ed68
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-10-23 23:55:15 +0900

    [Algorithm] - 프로그래머스 문자열 겹쳐쓰기

---
+++ Algorithm/problems/level0/StringOverwrite.java
@@ -0,0 +1,12 @@
+package algorithm.problems.level0;
+
+public class StringOverwrite {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = my_string.substring(0, s) + overwrite_string;
+
+        if(my_string.length() > answer.length()) {
+            answer += my_string.substring(answer.length());
+        }
+        return answer;
+    }
+}
