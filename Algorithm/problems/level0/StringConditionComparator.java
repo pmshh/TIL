07dd74ea2375811846bc815c478e1d96fa87e0d8
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-10-29 23:50:47 +0900

    [Algorithm] - 프로그래머스 조건 문자열

---
+++ Algorithm/problems/level0/StringConditionComparator.java
@@ -0,0 +1,20 @@
+package algorithm.problems.level0;
+
+public class StringConditionComparator {
+    public int solution(String ineq, String eq, int n, int m) {
+        if (ineq.equals(">")) {
+            if (eq.equals("=")) {
+                return n >= m ? 1 : 0;
+            } else if (eq.equals("!")) {
+                return n > m ? 1 : 0;
+            }
+        } else if (ineq.equals("<")) {
+            if (eq.equals("=")) {
+                return n <= m ? 1 : 0;
+            } else if (eq.equals("!")) {
+                return n < m ? 1 : 0;
+            }
+        }
+        return 0;
+    }
+}
