22d0053dabc252d6d9f1304479b0f3058befced7
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-11-07 23:51:35 +0900

    [Algorithm] - 프로그래머스 콜라츠 수열 만들기

---
+++ Algorithm/problems/level0/CollatzSequence.java
@@ -0,0 +1,26 @@
+package algorithm.problems.level0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollatzSequence {
+    public int[] solution(int n) {
+        List<Integer> sequence = new ArrayList<>();
+        sequence.add(n);
+
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            sequence.add(n);
+        }
+
+        int[] answer = new int[sequence.size()];
+        for (int i = 0; i < sequence.size(); i++) {
+            answer[i] = sequence.get(i);
+        }
+        return answer;
+    }
+}
