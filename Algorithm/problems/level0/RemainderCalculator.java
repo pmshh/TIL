1c0093bc5974322c8966c518d6afa46fcf3d268f
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-11-09 23:06:02 +0900

    [Algorithm] - 프로그래머스 나머지 구하기

---
+++ Algorithm/problems/level0/RemainderCalculator.java
@@ -0,0 +1,9 @@
+package algorithm.problems.level0;
+
+public class RemainderCalculator {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 % num2;
+        return answer;
+    }
+}
