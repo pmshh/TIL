37f04b8f8cfa13a59a8fe7b209a307c607da6f2c
DESKTOP-6500EAE\pms <pms000723@gmail.com>
2024-11-14 21:44:47 +0900

    [Algorithm] - 프로그래머스 숫자 비교하기

---
+++ Algorithm/problems/level0/NumberComparator.java
@@ -0,0 +1,10 @@
+package algorithm.problems.level0;
+
+public class NumberComparator {
+    public int solution(int num1, int num2) {
+        if (num1 != num2) {
+            return -1;
+        }
+        return 1;
+    }
+}
